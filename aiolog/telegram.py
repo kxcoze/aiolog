8979dc8ab05915e5c2cbc0aaa39e05339a04fb28
kxcoze <zakharloschev@gmail.com>
2023-01-12 16:38:32 +0300

    Added possibility to send logs for multiple chats.

--- aiolog/telegram.py
+++ aiolog/telegram.py
@@ -1,23 +1,27 @@
+from typing import List
+
 import aiohttp
 
 from . import base
 
 
 class Handler(base.Handler):
-    def __init__(self, token, chat_id, disable_notification=False, **kwargs):
+    """Added possibility to send logs for multiple chats"""
+    def __init__(self, token, chats, disable_notification=False, **kwargs):
         super().__init__(**kwargs)
-        self.chat_id = chat_id
+        self.chats: List[str] = [chat.strip() for chat in chats.split(',')]
         self.disable_notification = disable_notification
         self.url = "https://api.telegram.org/bot{}/sendMessage".format(token)
 
     async def store(self, entries):
         async with aiohttp.ClientSession() as session:
-            data = {
-                "chat_id": self.chat_id,
-                "text": "```\n{}\n```".format("\n".join(entries)),
-                "parse_mode": "markdown",
-                "disable_notification": self.disable_notification,
-                "disable_web_page_preview": True,
-            }
-            async with session.post(self.url, data=data):
-                pass
+            for chat in self.chats:
+                data = {
+                    "chat_id": chat,
+                    "text": "```\n{}\n```".format("\n".join(entries)),
+                    "parse_mode": "markdown",
+                    "disable_notification": self.disable_notification,
+                    "disable_web_page_preview": True,
+                }
+                async with session.post(self.url, data=data):
+                    pass
